fc81ed30db08c8a4dad647425605e042b25e063a
asraraa <71617480+asraraa@users.noreply.github.com>
2022-09-11 11:21:15 +0700

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a0)1w6=+$kh0ai(7o@l46v25d)k+efig574h39nqn-8pk7zl5_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sisipedi.herokuapp.com']
 
