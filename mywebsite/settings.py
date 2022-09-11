a70bd4587a33a93526b7b5a50737d8d34555e80b
asraraa <71617480+asraraa@users.noreply.github.com>
2022-09-11 11:37:41 +0700

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a0)1w6=+$kh0ai(7o@l46v25d)k+efig574h39nqn-8pk7zl5_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sisipedi.herokuapp.com']
 
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -123,17 +122,15 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATIC_URL = "static/"
-
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
 django_on_heroku.settings(locals())
 
-STATICFILES_DIRS = [
-    #os.path.join(BASE_DIR,'static')
-    BASE_DIR / "static",
-]
+
+#if DEBUG:
+ #       STATICFILES_DIRS = [ BASE_DIR / "static",]
+#else:
+        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
