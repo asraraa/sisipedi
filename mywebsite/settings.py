fd3a40c9f3a117e98b5dc209b0a96e81e8c920a6
asraraa <71617480+asraraa@users.noreply.github.com>
2022-09-11 10:40:27 +0700

    Add files via upload

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['sisipedi.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
